5d84cf30aa27ed68e0a2b0c6388d12bae544013c
AnnaMut <butorovaanna@yandex.ru>
2018-04-26 01:20:12 +0300

    исправлено движение

--- js/map.js
+++ js/map.js
@@ -17,8 +17,8 @@ var GUESTS_MAX = 20;
 var PIN_WIDTH = 40;
 var PIN_HEIGHT = 44;
 var MAX_PRICE = 1000000;
-var MAIN_PIN_WIDTH = 62;
-var MAIN_PIN_HEIGHT = 62;
+var TAIL_SHIFT_X = 31;
+var TAIL_SHIFT_Y = 77;
 
 var offerTitles = [
   'Большая уютная квартира',
@@ -74,6 +74,7 @@ var MapCoords = {
   LEFT: 0,
   RIGHT: 1100
 };
+
 var startCoords = {};
 
 var pinPrefix = 'pin-';
@@ -284,11 +285,18 @@ var getPins = function () {
   pinsBox.appendChild(pinFragment);
 };
 
+var fillAddressCoords = function (x, y) {
+  var positionX = parseInt(x, 10);
+  var positionY = parseInt(y, 10);
+  addressPart.value = positionX + ', ' + positionY;
+}; // функция сделала без смещения, так как дальше не стыковка была
+
 var mainPinMouseUpHandler = function () {
   closePageOverlay();
   getActiveFieldsets();
   getPins();
   mainPin.removeEventListener('mouseup', mainPinMouseUpHandler);
+  fillAddressCoords(mainPin.offsetLeft + TAIL_SHIFT_X, mainPin.offsetTop + TAIL_SHIFT_Y);// передаем параменты со смещением
 };
 
 var getActiveFieldsets = function () {
@@ -414,12 +422,6 @@ var resetFormClickHandler = function () {
   mainPin.addEventListener('mouseup', mainPinMouseUpHandler);
 };
 
-var fillAddressCoords = function (x, y) {
-  var positionX = parseInt(x, 10);
-  var positionY = parseInt(y, 10) + MAIN_PIN_HEIGHT / 2;
-  addressPart.value = positionX + ', ' + positionY;
-};
-
 var mouseMoveHandler = function (moveEvt) {
   moveEvt.preventDefault();
 
@@ -429,28 +431,32 @@ var mouseMoveHandler = function (moveEvt) {
   };
 
   startCoords = {
-    x: moveEvt.clientX,
+    x: moveEvt.clientX, // тут смещение по пину не нужно
     y: moveEvt.clientY
   };
 
-  fillAddressCoords(mainPin.offsetLeft, mainPin.offsetTop);
-
   var pinPosition = {
-    top: mainPin.offsetTop - shift.y,
-    left: mainPin.offsetLeft - shift.x
+    left: mainPin.offsetLeft - shift.x, // тут смещение по пину не нужно
+    top: mainPin.offsetTop - shift.y
   };
 
   var limitPosition = function (element, min, max) {
     return Math.min(Math.max(element, min), max);
   };
 
-  pinPosition.left = limitPosition(pinPosition.left, MapCoords.LEFT + Math.ceil(MAIN_PIN_WIDTH / 2), MapCoords.RIGHT - Math.ceil(MAIN_PIN_WIDTH / 2));
-  pinPosition.top = limitPosition(pinPosition.top, MapCoords.TOP - Math.ceil(MAIN_PIN_HEIGHT / 2), MapCoords.BOTTOM - Math.ceil(MAIN_PIN_HEIGHT / 2));
+  pinPosition.left = limitPosition(pinPosition.left, MapCoords.LEFT, MapCoords.RIGHT); // тут ограничиваем движение
+  pinPosition.top = limitPosition(pinPosition.top, MapCoords.TOP, MapCoords.BOTTOM);
 
-  mainPin.style.top = pinPosition.top + 'px';
   mainPin.style.left = pinPosition.left + 'px';
-  fillAddressCoords(mainPin.style.top, mainPin.style.left);
-};
+  mainPin.style.top = pinPosition.top + 'px';
+
+  var newAddresscoors = {
+    x: pinPosition.left + TAIL_SHIFT_X + 'px',
+    y: pinPosition.top + 'px',
+  };
+  fillAddressCoords(newAddresscoors.x, newAddresscoors.y); // сюда передали посчитанное с учетом пина
+};// получилось что когда пин стоит в левом нижнем углу то координаты его 31 и 500 как нам и надо
+// середина и острый конец.
 
 var mouseUpHandler = function (upEvt) {
   upEvt.preventDefault();
@@ -465,9 +471,10 @@ var mainPinMouseDownHandler = function (downEvt) {
   };
   document.addEventListener('mousemove', mouseMoveHandler);
   document.addEventListener('mouseup', mouseUpHandler);
+  fillAddressCoords(startCoords.x, startCoords.y);
 };
 
-mainPin.addEventListener('mouseup', mainPinMouseUpHandler);// удалила на строке 284
+mainPin.addEventListener('mouseup', mainPinMouseUpHandler);
 
 formTitle.addEventListener('blur', formTitleBlurHandler);
 formTitle.addEventListener('focus', formTitleFocusHandler);
@@ -479,6 +486,5 @@ formTimeOutSelect.addEventListener('change', formTimeOutChangeHandler);
 formTimeInSelect.addEventListener('change', formTimeInChangeHandler);
 formSubmitButton.addEventListener('click', formSubmitButtonClickHandler);
 formResetButton.addEventListener('click', resetFormClickHandler);
-
 
 mainPin.addEventListener('mousedown', mainPinMouseDownHandler);
